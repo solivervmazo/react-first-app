94031dcbe8d0b70d914e84139e94f94478ffe11e
LAPTOP-Q8PH4MAI\PC <solivervmazo.jobs@gmail.com>
2023-10-08 19:20:41 +0800

    use rapid api

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,10 +11,13 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: 1,
+  });
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -30,10 +33,10 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+            data={data}
             renderItem={(item) => (
               <PopularJobCard
-                item={item}
+                item={item.item}
                 keyExtractor={(item) => item?.job_id}
               />
             )}
