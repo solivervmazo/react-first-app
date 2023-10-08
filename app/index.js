4c8039c9b6eb78130ec9b8f6b8c701180a7ac780
LAPTOP-Q8PH4MAI\PC <solivervmazo.jobs@gmail.com>
2023-10-08 15:35:56 +0800

    Home Screen

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { ScrollView, SafeAreaView } from "react-native";
+import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
 import { COLORS, icons, images, SIZES } from "../constants";
 import {
   Nearbyjobs,
   Popularjobs,
-  SceenHeaderBtn,
+  ScreenHeaderBtn,
   Welcome,
 } from "../components";
 
@@ -15,8 +15,30 @@ const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
-        options={{ headerStyle: { backgroundColor: COLORS.lightWhite } }}
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="60%" />
+          ),
+          headerTitle: "",
+        }}
       />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome></Welcome>
+          <Popularjobs></Popularjobs>
+          <Nearbyjobs></Nearbyjobs>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
